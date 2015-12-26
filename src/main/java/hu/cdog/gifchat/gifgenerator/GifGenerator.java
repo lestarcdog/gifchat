31391b9a84ac24aa0e172800421b988caa9d312f
cdog <cdog@cdogmainframe>
2015-12-26 11:26:29 +0100

    test for drawing test on images

--- src/main/java/hu/cdog/gifchat/gifgenerator/GifGenerator.java
+++ src/main/java/hu/cdog/gifchat/gifgenerator/GifGenerator.java
@@ -1,10 +1,14 @@
 package hu.cdog.gifchat.gifgenerator;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
@@ -46,7 +50,13 @@ public class GifGenerator {
 
 		ObjectMapper mapper = new ObjectMapper();
 		GiphyData gipyData = mapper.readValue(rawContent, GiphyData.class);
+		URL gifUrl = new URL(gipyData.getData().get(0).getImages().getDownsized().getUrl());
+		BufferedImage gif = ImageIO.read(gifUrl);
+		ImageReader reader = ImageIO.getImageReadersBySuffix("gif").next();
+
+		// reader.
 		// System.out.println(gipyData.getData().get(0).getImages().getDownsized().getUrl());
+
 	}
 
 }
--- src/test/java/hu/cdog/gifchat/gifgenerator/GifGeneratorTest.java
+++ src/test/java/hu/cdog/gifchat/gifgenerator/GifGeneratorTest.java
@@ -1,6 +1,16 @@
 package hu.cdog.gifchat.gifgenerator;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
 
 import org.junit.Test;
 
@@ -11,10 +21,29 @@ public class GifGeneratorTest {
 
 	@Test
 	public void test() throws JsonParseException, JsonMappingException, IOException {
-		GifGenerator gg = new GifGenerator();
-		gg.init();
+		InputStream testInput = GifGeneratorTest.class.getResourceAsStream("/test.gif");
+		String text = "anyad";
+		ImageReader reader = ImageIO.getImageReadersBySuffix("gif").next();
+		reader.setInput(ImageIO.createImageInputStream(testInput));
 
-		gg.downloadFirst("cat");
+		System.out.println(reader.getNumImages(true));
+		int num = reader.getNumImages(true);
+		ImageWriter writer = ImageIO.getImageWriter(reader);
+		IIOMetadata imageMetadata = reader.getImageMetadata(0);
+		writer.setOutput(ImageIO.createImageOutputStream(new File("anyad.gif")));
+		writer.prepareWriteSequence(imageMetadata);
+		for (int i = 0; i < num; i++) {
+
+			BufferedImage originalImg = reader.read(i, null);
+			Graphics2D graphics = originalImg.createGraphics();
+			graphics.drawString(text, 10.0f, 10.0f);
+			writer.writeToSequence(new IIOImage(originalImg, null, imageMetadata), null);
+
+		}
+		writer.endWriteSequence();
+		writer.dispose();
+		reader.dispose();
+
 	}
 
 }
